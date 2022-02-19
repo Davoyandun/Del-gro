f13a317f72d0e398b8ad5046c6a9ab1df4f31833
David Yandún <70674906+Davoyandun@users.noreply.github.com>
2022-02-18 20:02:27 -0500

    inserte iconos de redes sociales en page de contacts

--- front/src/pages/Contacts.jsx
+++ front/src/pages/Contacts.jsx
@@ -1,13 +1,34 @@
 import React from "react";
+import { FaFacebookSquare, FaLinkedin, FaWhatsapp } from "react-icons/fa";
 import FormContacts from "../components/FormContacts";
-
 
 export default function Contacts() {
   return (
     <div>
       <h3>Contactos</h3>
-      <p>Para nosotros es importante tu opinión es importante. </p>
-     <FormContacts/>
+      <h4>Para nosotros la estar siempre cerca es muy importante </h4>
+      <div>
+        <p>Para contactarse con la empresa, aqui puedes enviar un correo</p>
+        <FormContacts />
+      </div>
+      <div>
+        <p>También nos encuentras en las redes sociales</p>
+        <a href="">
+          <FaFacebookSquare />
+        </a>
+        <a href="">
+          <FaLinkedin />
+        </a>
+        <a href="">
+          <FaWhatsapp />
+        </a>
+      </div>
+      <div>
+        <p>
+          O si prefieres, puedes escribirnos directamente a nuestro correo:
+          delagro2016@gmail.com
+        </p>
+      </div>
     </div>
   );
 }
